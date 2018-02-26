695d835b99c27ed9171ba36af425cbaf7328aae9
Your Name <you@example.com>
2018-02-26 12:08:03 +0100

    Opgave 2 lektion 4 til og med c

---
+++ Opgave 2 lektion 4/adder.asm
@@ -0,0 +1,33 @@
+.include "m32def.inc"
+
+	rjmp	RESET
+
+;Initialisér programmet
+RESET:
+
+; PORTC setup
+	ldi		R16, 0x00			;
+	out		DDRC, R16			; Set PORTC as input
+	ldi		R16, 255			;
+	out 	PORTC,R16 		; Enable pull-up on PORTC
+
+  ; PORTB setup
+  	out 	DDRB,R16 			; PORTB = output R16 is high from line 8
+    rjmp	LOOP ; Start loop
+
+LOOP:
+
+in R16,PINC ; Get pins
+out PORTB,R16
+
+; Hex values for display
+; 1. seg = 11111110 = 0xFE
+; 2. seg = 11111101 = 0xFD
+; 3. Seg = 11111011 = 0xFB
+; 4. Seg = 11110111 = 0xF7
+; 5. seg = 11101111 = 0xEF
+; 6. seg = 11011111 = 0xDF
+; 7. seg = 10111111 = 0xBF
+; Punktum = 01111111 = 0x7F
+
+rjmp LOOP
